c80ffcddf072416f191399ab5470ce925c8552fc
Oscar Dowson <o.dowson@gmail.com>
2018-10-02 22:31:20 +0000

    Automatic update script.

--- daily_update.sh
+++ daily_update.sh
@@ -10,6 +10,9 @@ git pull
 python3 src/scrape_data.py >> 'docs/log.txt'
 python3 src/model.py >> 'docs/log.txt'
 
+cat data/raw/nzx/NZXMKP-* | sort | 
+  tail -n +$(ls data/raw/nzx/NZXMKP-* | wc -l) > data/processed/nzx_mkp.csv
+
 # Push any changes to Github.
 git add .
 git commit -m "Automatic update script."
