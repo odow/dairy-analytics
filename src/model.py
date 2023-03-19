a5d506c78befdac48301e2d303b80e4f7a37a261
odow <o.dowson@gmail.com>
2023-03-20 12:50:57 +1300

    Prep for public release

--- src/model.py
+++ src/model.py
@@ -1,3 +1,9 @@
+#  Copyright 2015-2023, Oscar Dowson.
+#
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 MODEL_VERSION = 1
 
 RAW_GDT_DATA = 'data/raw/gdt/'
